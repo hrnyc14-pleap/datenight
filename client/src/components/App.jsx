fdac8086c45a08a4cb6398c9701508cf67e438ef
Amy San Felipe <amysanfelipe@gmail.com>
2018-04-10 14:45:31 -0400

    Set up React router and component pages

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Main from './main.jsx';
+import Home from './home.jsx';
+
+const App = () => (
+  <div>
+    <Main />
+  </div>
+)
+
+
+export default App;
---
+++ client/src/components/Results.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>Hello</div>
+    )
+  }
+}
+
+export default Results;
