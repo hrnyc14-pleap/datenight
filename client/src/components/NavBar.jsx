cc2ac268f1dc45511c395a6d563fd473e066073b
Ian Pradhan <Ian>
2018-04-12 10:25:57 -0400

    Work on navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,11 +5,11 @@ const NavBar = ({path, handleLogout}) => (
     <div>
         Navigation bar
         {console.log('THIS IS THE PATH', path)}
-        {path !== '/signup' && <Link to='/login'>Login</Link>}
-        {path !== '/login' && <Link to='/signup'>Sign up</Link>}
-        {(path === '/login' || path === '/signup' ) && <Link to='/questions'>Questions</Link>}
-        {(path === '/login' || path === '/signup' ) && <Link to='/favorites'>Favorites</Link>}
-        {(path === '/login' || path === '/signup' ) && <button onClick={handleLogout}> logout </button>}
+        {!(path === '/signup') && <Link to='/login'>Login</Link>}
+        {!(path === '/login') && <Link to='/signup'>Sign up</Link>}
+        {!(path !== '/login' && path !== '/signup' ) && <Link to='/questions'>Questions</Link>}
+        {!(path !== '/login' && path !== '/signup' ) && <Link to='/favorites'>Favorites</Link>}
+        {!(path !== '/login' && path !== '/signup' ) && <button onClick={handleLogout}> logout </button>}
     </div>
 )
 
