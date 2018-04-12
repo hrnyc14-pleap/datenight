49bf3c3f7dae3c614a38cdd391faeebaf9f5966e
Heidi Poon <heidixpoon@gmail.com>
2018-04-12 15:43:44 -0400

    added material ui

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+
 
 const NavBar = ({path, handleLogout}) => (
     <div>
-        Navigation bar
-        {console.log('THIS IS THE PATH', path)}
-        {(path === '/signup' || path === '/') && <Link to='/login'>Login</Link>}
-        {(path === '/login') && <Link to='/signup'>Sign up</Link>}
-        {(path !== '/login' && path !== '/signup' && path !== '/') && <Link to='/questions'>Questions</Link>}
-        {(path !== '/login' && path !== '/signup' && path !== '/' ) && <Link to='/favorites'>Favorites</Link>}
-        {(path !== '/login' && path !== '/signup' && path !== '/' ) && <button onClick={handleLogout}> logout </button>}
+        <AppBar
+          title="Date Night Generator"
+          iconElementRight={
+                <div>
+                    {console.log('THIS IS THE PATH', path)}
+                    {(path === '/signup' || path === '/') && <Link to='/login'><FlatButton label="Login" style={{"color":"white"}}/></Link>}
+                    {(path === '/login') && <Link to='/signup'><FlatButton label="Sign Up" style={{"color":"white"}}/></Link>}
+                    {(path !== '/login' && path !== '/signup' && path !== '/') && <Link to='/questions'><FlatButton label="Questions" style={{"color":"white"}}/></Link>}
+                    {(path !== '/login' && path !== '/signup' && path !== '/' ) && <Link to='/favorites'><FlatButton label="Favorites" style={{"color":"white"}}/></Link>}
+                    {(path !== '/login' && path !== '/signup' && path !== '/' ) && <FlatButton onClick={handleLogout} label="Log Out" style={{"color":"white"}}/>}
+
+                </div>
+        
+            }
+        />
+       
     </div>
 )
 
