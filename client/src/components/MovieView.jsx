0a96c11de5e988019fb4e437cbfce13b6e61c667
Amy San Felipe <amysanfelipe@gmail.com>
2018-04-13 16:21:40 -0400

    style changes

--- client/src/components/MovieView.jsx
+++ client/src/components/MovieView.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const MovieView = ({data}) => (
-    <div>
+    <div className="view">
         <h3> {data.title} </h3>
-        <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}/>
+        <img style={{height: '500px', width: 'auto'}} src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}/>
     </div>
 )
 
